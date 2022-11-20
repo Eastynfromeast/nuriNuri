1af9b5de470521f0c80338bf5fde87cd5a85a7e3
Eastyn <eastynwork@gmail.com>
2022-11-20 22:47:49 +0900

    Initialized the app with Nuri's image

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        appBar: AppBar(
+          title: const Text('Nuri Nuri'),
+          backgroundColor: Colors.amberAccent[700],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/nuri_main.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'nuriNuri',
+//     );
+//   }
+// }
+
+// @override
+// Widget build(BuildContext context) {
+//   // This method is rerun every time setState is called, for instance as done
+//   // by the _incrementCounter method above.
+//   //
+//   // The Flutter framework has been optimized to make rerunning build methods
+//   // fast, so that you can just rebuild anything that needs updating rather
+//   // than having to individually change instances of widgets.
+//   return Scaffold(
+//     appBar: AppBar(
+//       // Here we take the value from the MyHomePage object that was created by
+//       // the App.build method, and use it to set our appbar title.
+//       title: const Text('nuriNuri'),
+//     ),
+//     body: Center(
+//       // Center is a layout widget. It takes a single child and positions it
+//       // in the middle of the parent.
+//       child: Column(
+//           // Column is also a layout widget. It takes a list of children and
+//           // arranges them vertically. By default, it sizes itself to fit its
+//           // children horizontally, and tries to be as tall as its parent.
+//           //
+//           // Invoke "debug painting" (press "p" in the console, choose the
+//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
+//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+//           // to see the wireframe for each widget.
+//           //
+//           // Column has various properties to control how it sizes itself and
+//           // how it positions its children. Here we use mainAxisAlignment to
+//           // center the children vertically; the main axis here is the vertical
+//           // axis because Columns are vertical (the cross axis would be
+//           // horizontal).
+
+//           ),
+//     ),
+//     // This trailing comma makes auto-formatting nicer for build methods.
+//   );
+// }
